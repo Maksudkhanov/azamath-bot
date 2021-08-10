e4c7a364278e85af23e798671cf032974aa35961
Maksudkhanov <azizxon.maksudxanov@mail.ru>
2021-08-10 18:02:57 +0500

    more beatiful output of result

--- src/processAnswers/checkTest.js
+++ src/processAnswers/checkTest.js
@@ -2,7 +2,8 @@ const correctAnswers = require('../test/correctAnswers').module
 const ids = [];
 
 function checkAnswer(answer, userId) {
-    let result = 'Результат \n'
+    let result = []
+
     
     if(ids.includes(userId)) {
         return 'Вы уже отправили ответы'
@@ -13,15 +14,23 @@ function checkAnswer(answer, userId) {
     let numberOfCorrect = 0
     for(let i = 0; i < correctAnswers.length; i++) {
         if(correctAnswers[i] === answer[i].toLowerCase()) {
-            result += [i+1] + '. ✅ \n';
+            result[i] = '✅';
             numberOfCorrect++;
         } else {
-            result += [i+1] + '. ❌\n';
+            result[i] = '❌';
         }
     }
-    result+= '\nКоличество правильных: ' + numberOfCorrect
+
+    const results = 
+`Результат
+1. ${result[0]}     6. ${result[5]}     11. ${result[10]}     16. ${result[15]}
+2. ${result[1]}     7. ${result[6]}     12. ${result[11]}     17. ${result[16]}
+3. ${result[2]}     8. ${result[7]}     13. ${result[12]}     18. ${result[17]}
+4. ${result[3]}     9. ${result[8]}     14. ${result[13]}     19. ${result[18]}
+5. ${result[4]}    10. ${result[9]}    15. ${result[14]}     20. ${result[19]}
+\nКоличество правильных: ${numberOfCorrect}`
     
-    return result.bold()
+    return results.bold()
 }
 
 exports.module = checkAnswer;
